7a96d1b7b8c46c211ac9ef8512ffdcd9141de15a
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2024-01-17 07:00:33 +0100

    update changes

--- hardhat-fund-me/contracts/FundMe.sol
+++ hardhat-fund-me/contracts/FundMe.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: MIT
-// 1. Pragma
+
 pragma solidity ^0.8.7;
-// 2. Imports
+
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-// 3. Interfaces, Libraries, Contracts
 error FundMe__NotOwner();
 
 /**@title A sample Funding Contract
- * @author Patrick Collins
+ * @author Chuck Joshua M.
  * @notice This contract is for creating a sample funding contract
  * @dev This implements price feeds as our library
  */
@@ -32,16 +31,6 @@ contract FundMe {
         if (msg.sender != i_owner) revert FundMe__NotOwner();
         _;
     }
-
-    // Functions Order:
-    //// constructor
-    //// receive
-    //// fallback
-    //// external
-    //// public
-    //// internal
-    //// private
-    //// view / pure
 
     constructor(address priceFeed) {
         s_priceFeed = AggregatorV3Interface(priceFeed);
