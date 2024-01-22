9f42781dbd5fe4a935824534abc19b8424859fd9
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2024-01-22 06:36:37 +0100

    fix minor changes

--- hardhat-fund-me/test/unit/FundMe.test.js
+++ hardhat-fund-me/test/unit/FundMe.test.js
@@ -1,10 +1,11 @@
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 const { deployments, ethers, getNamedAccounts } = require("hardhat")
 
 describe("FundMe", async () => {
     let fundMe
     let deployer
     let mockV3Aggregator
+    const sendValue = ethers.utils.parseEther("50")
     beforeEach(async () => {
         // const accounts = await ethers.getSigners()
         // const accountZero = accounts[0]
@@ -21,6 +22,20 @@ describe("FundMe", async () => {
         it("sets the aggregator addresses correctly", async () => {
             const response = await fundMe.getPriceFeed()
             assert.equal(response, mockV3Aggregator.address)
+        })
+    })
+
+    describe("funds", async () => {
+        it("Fails if you don't send enough ETH", async () => {
+            await expect(fundMe.fund()).to.be.revertedWith(
+                "You need to spend more ETH"
+            )
+        })
+        it("update the amount funded data structure", async () => {
+            // await fundMe.fund({ value: sendValue })
+            await fundMe.fund({ value: sendValue, gasLimit: 2000000 })
+            const response = await fundMe.getAddressToAmountFunded(deployer)
+            assert.equal(response.toString(), sendValue.toString())
         })
     })
 })
